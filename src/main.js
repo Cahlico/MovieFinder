6ad859a876f8c076d626b7e1086547f0644424b5
Carlos <carlosaugusto19991@poli.ufrj.br>
2020-10-01 11:46:05 -0300

    request dos filmes com separacao por categorias

--- src/main.js
+++ src/main.js
@@ -1,17 +1,44 @@
-const key = "921ca98e7932d5a487ee256da6405b38";
+import sendRequestPopular from "./popular.js"
+import sendRequestNowPlaying from "./now_playing"
+import sendRequestTopRated from "./top_rated"
+import sendRequestUpcoming from "./upcoming"
+
 var chosenBefore = document.querySelector("button:first-child");
 
+sendRequestPopular();
 addEventOnNavigator();
 
 function addEventOnNavigator() {
     
     var button = document.querySelectorAll("button");
-    button.forEach((element, index) => element.addEventListener("click", setPage)); 
+    button.forEach((element) => element.addEventListener("click", setCategory));
 }
 
-function setPage() {
+function setCategory() {
 
     event.target.setAttribute("class", "chosen");
     chosenBefore.removeAttribute("class");
+
     chosenBefore = event.target;
+    setPage(event.target);
+}
+
+function setPage(target) {
+
+    var erase = document.querySelector("main");
+    erase.remove();
+
+    var page = target.innerText;
+
+    if(page === "Populares") {
+
+        sendRequestPopular();
+    } else if (page === "Nos cinemas") {
+
+        sendRequestNowPlaying();
+    } else if (page === "Maiores notas") {
+        sendRequestTopRated();
+    } else {
+        sendRequestUpcoming();
+    }
 }
--- src/upcoming.js
+++ src/upcoming.js
@@ -1,8 +1,8 @@
 const key = "921ca98e7932d5a487ee256da6405b38";
 
-export default function sendRequestPopular() {
+export default function sendRequestUpcoming() {
 
-    var request = axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=${key}`);
+    var request = axios.get(`https://api.themoviedb.org/3/movie/upcoming?api_key=${key}`);
     request.then(receiveData);
 }
 
@@ -16,7 +16,7 @@ function renderPage(data) {
 
     var body = document.querySelector("body");
     var container = document.createElement("main");
-    
+
     data.forEach(element => {
         container.innerHTML += `<img id="${element.id}" 
         src="https://image.tmdb.org/t/p/w500${element.poster_path}" 
